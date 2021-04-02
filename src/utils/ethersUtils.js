937670bad3fc58cfd32f92502f40c1660419f220
leon <leon.ji@tron.network>
2021-04-02 15:29:48 +0800

    upgrade to 3.2.6

--- src/utils/ethersUtils.js
+++ src/utils/ethersUtils.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // import {utils} from 'ethers';
 
@@ -15,24 +14,12 @@ import {AbiCoder} from 'ethers/utils/abi-coder';
 const recoverAddress = ''
 const SigningKey = ''
 const toUtf8Bytes = ''
-=======
-import { utils } from "ethers";
-
-const keccak256 = utils.keccak256;
-const sha256 = utils.sha256;
-const toUtf8Bytes = utils.toUtf8Bytes;
-const toUtf8String = utils.toUtf8String;
-const recoverAddress = utils.recoverAddress;
-const SigningKey = utils.SigningKey;
-const AbiCoder = utils.AbiCoder;
->>>>>>> origin/master
 
 export {
     keccak256,
     sha256,
     toUtf8Bytes,
-    toUtf8String,
     recoverAddress,
     SigningKey,
-    AbiCoder,
+    AbiCoder
 };
